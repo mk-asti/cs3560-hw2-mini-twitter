8f0b2d2fd12c43254479c8c3ab94a70de22ff76b
mk-asti <marienkaylaa@gmail.com>
2026-07-01 21:15:51 -0700

    fixed post statistics logic

--- src/admin_service/UserStatsVisitor.java
+++ src/admin_service/UserStatsVisitor.java
@@ -19,23 +19,12 @@ public class UserStatsVisitor implements Visitor {
 	
 	private int totalUserCount = 0;
 	private int totalGroupCount = 0;
-	private int totalPostCount = 0;
-	private int totalPositivityCount = 0;
 
 	
 	// visits user node
 	@Override
 	public void visit(User user) {
 		totalUserCount++;	// increment total number of users
-		
-		List<NewPost> userFeed = user.getUserFeed().getPostHistory();
-		totalPostCount += userFeed.size();	// increment post count
-		
-		for(NewPost tweet : userFeed) {
-			if(checkPositive(tweet.getText())){
-				totalPositivityCount++;		// increment post with positive words count
-			}
-		}
 	}
 
 	// visits usergroup node
@@ -59,52 +48,16 @@ public class UserStatsVisitor implements Visitor {
 	}
 
 	public int getPostCount() {
-		
-		return totalPostCount;
-	}
-
-	public int getPositivityCount() {
-		
-		return totalPositivityCount;
-	}
-	
-	
-	// helper method: check for positive words
-	private boolean checkPositive(String postText) {
-		String text = postText.toLowerCase();
-		for(String word : listPositiveWords()) {
-			if(text.contains(word)) {
-				return true;
-			}
-		}
-		
-		return false;
-	}
-	
-	// helper method: create list of positive words
-	private List<String> listPositiveWords(){
-		List<String> pos = new ArrayList<>();
-		
-		pos.add("good");
-		pos.add("happy");
-		pos.add("thank");
-		pos.add("thankful");
-		pos.add("great");
-		pos.add("fire");
-		pos.add("bet");
-		pos.add("sick");
-		pos.add("cool");
-		
-		return pos;
+	    return NewPost.getTotalPostCount();
 	}
 	
 	// helper method: calculate positivity post percentage
 	public double calculatePercentage() {
-		if(totalPostCount == 0) {
-			return 0.0;
-		}
-		
-		return ((double)totalPositivityCount / totalPostCount) * 100;
+	    if(NewPost.getTotalPostCount() == 0) {
+	    	return 0.0;
+	    }
+	    
+	    return ((double) NewPost.getTotalPositiveCount() / NewPost.getTotalPostCount()) * 100;
 	}
 	
 }
